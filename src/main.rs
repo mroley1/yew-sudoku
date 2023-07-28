807647c1c1a0db8c52e4d79baacc6c163f7f433e
mroley1 <64291507+mroley1@users.noreply.github.com>
2023-07-27 20:32:17 -0400

    reduce?

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use weblog::console_log;
 
 use yew::prelude::*;
 
+// trunk serve --open
+
+
 #[derive(Clone, PartialEq)]
 struct OlItem {
     id: u8,
@@ -53,15 +56,38 @@ fn item_details(ItemDetailsProps { item }: &ItemDetailsProps) -> Html {
 
 #[derive(Clone, PartialEq)]
 struct Cell {
-    x: u8,
-    y: u8,
-    value: u8,
-    potential: Vec<u8>,
+    x: usize,
+    y: usize,
+    value: usize,
+    potential: Vec<usize>,
 }
 
+#[derive(Properties, PartialEq)]
+struct CellProps {
+    items: Vec<Cell>,
+    on_click: Callback<Cell>,
+}
+
+#[function_component(Grid)]
+fn item_list(CellProps { items, on_click }: &CellProps) -> Html {
+    let on_click = on_click.clone();
+    items.iter().map(|item| {
+        let on_item_select = {
+            let on_click = on_click.clone();
+            let item = item.clone();
+            Callback::from(move |_| {
+                on_click.emit(item.clone())
+            })
+        };
+        
+        html! {
+            <li key={item.x} onclick={on_item_select}>{item.value.clone()}</li>
+        }
+    }).collect()
+}
 
 impl Cell {
-    fn new(x: u8, y: u8) -> Cell {
+    fn new(x: usize, y: usize) -> Cell {
         Cell {x, y, value: 0, potential: vec![]}
     }
 }
@@ -85,73 +111,98 @@ fn get_clean_board() -> Board {
     }
 }
 
+enum BoardAction {
+    Set,
+}
+
 struct Board {
     grid: Array2D<Cell>,
     solved: bool,
+}
+
+impl Default for Board {
+    fn default() -> Self {
+        let mut long_vec = vec![];
+        for i in 0..=80 {
+            let x = i % 9;
+            let y = i / 9;
+            long_vec.push(Cell::new(x, y));
+    }
+        Self {
+            grid: Array2D::from_row_major(&long_vec, 9, 9).unwrap(),
+            solved: false,
+        }
+    }
+}
+
+impl Reducible for Board {
+    type Action = BoardAction;
+    fn reduce(self: std::rc::Rc<Self>, action: Self::Action) -> std::rc::Rc<Self> {
+        let mut next_grid = match action {
+            BoardAction::Set => self.grid.clone(),
+        };
+        
+        next_grid.get_mut(0, 0).unwrap().value = 4;
+        
+        Self {grid: next_grid, solved: self.solved}.into()
+    }
 }
 
 impl Board {
     fn get_cell(&self, x: usize, y: usize) -> Cell {
         return self.grid.get(y, x).expect("invalid get").clone();
     }
-    fn set_val(&mut self, x: usize, y: usize, value: u8) {
+    fn set_val(&mut self, x: usize, y: usize, value: usize) {
         self.grid.get_mut(y, x).unwrap().value = value;
     }
 }
 
+
 #[function_component(App)]
 fn app() -> Html {
-    let items = vec![
-        OlItem {
-            id: 1,
-            content: String::from("red"),
-            styles: String::from("color: red"),
-        },
-        OlItem {
-            id: 2,
-            content: String::from("green"),
-            styles: String::from("color: green; transform: rotate(-30deg);"),
-        },
-        OlItem {
-            id: 3,
-            content: String::from("yeller"),
-            styles: String::from("color: goldenrod"),
-        },
-        OlItem {
-            id: 4,
-            content: String::from("blue"),
-            styles: String::from("color: blue"),
-        },
-    ];
     
 
-    let mut grid = get_clean_board();
+    //let grid = get_clean_board();
+
+
     
-    console_log!(grid.get_cell(1, 3).x);
-    console_log!(grid.get_cell(1, 3).y);
-    grid.set_val(1, 3, 4);
-    console_log!(grid.get_cell(1, 3).value);
+    let board = use_reducer(Board::default);
     
-    let selected_item = use_state(|| None);
+    // let on_item_select = {
+    //     let selected_item = selected_item.clone();
+    //     Callback::from(move |item: OlItem| {
+    //         selected_item.set(Some(item))
+    //     }) 
+    // };
     
-    let on_item_select = {
-        let selected_item = selected_item.clone();
-        Callback::from(move |item: OlItem| {
-            selected_item.set(Some(item))
-        }) 
+    // let click_cell = {
+    //     console_log!(board.borrow_mut().get_cell(0, 0).value);
+    //     let board = board.clone();
+    //     Callback::from(move |item: Cell| {
+    //         board.borrow_mut().set_val(item.x, item.y, 3);
+    //     }) 
+    // };
+    
+    let click_click = {
+        let board = board.clone();
+        Callback::from(move |_| board.dispatch(BoardAction::Set))
     };
     
-    let details = selected_item.as_ref().map(|item| html! {
-        <ItemDetails item={item.clone()} />
-    });
+    
+    
+    // let details = selected_item.as_ref().map(|item| html! {
+    //     <ItemDetails item={item.clone()} />
+    // });
     
     html! {
         <>
-            <ol>
-                <ItemList items={items} on_click={on_item_select.clone()} />
-            </ol>
-            {for details}
-            <img src="https://assets.mroley.dev/discordBG.jpg" alt="picture" />
+            <div style="background-color: green; width: 100%; height: 100%; display: flex; justify-content: center">
+                <div style="aspect-ratio: 1; height: 100%; background-color: aqua;">
+                    <ol>
+                        <Grid items={board.grid.as_column_major()} on_click={click_click} />
+                    </ol>
+                </div>
+            </div>
         </>
     }
 }
